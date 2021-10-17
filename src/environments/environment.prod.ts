8091ed93dfba0e9e9359530d919a0894ce52075a
luccsdev <80589359+luccsdev@users.noreply.github.com>
2021-10-17 16:59:05 -0300

    environment atualizacao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true ,
+  apiURLBase: 'http://localhost:8080',
+  clientId: 'my-angular-app',
+  clientSecret:'@321',
+  obterTokenUrl: '/oauth/token'
 };
+
